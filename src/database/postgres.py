6c2e7b6369ef91e4c86a59da325d687989a58967
Алексей Фадеев <a.fadeev@dataintegral.ru>
2025-06-15 11:37:18 +0300

    rewrite inserting

--- src/database/postgres.py
+++ src/database/postgres.py
@@ -1,5 +1,7 @@
 import psycopg2
+from psycopg2 import extras
 from contextlib import closing
+from typing import Union, List, Tuple, Dict
 
 from utils.base import get_logger
 
@@ -17,9 +19,9 @@ class PostgresDB:
             'dbname': 'users'
         }
 
-    def _execute_query(self, query: str) -> None:
+    def _execute_batch(self, query: str, records: Union[List[Tuple], List[Dict]]) -> None:
         with closing(psycopg2.connect(**self.conn_params)) as pg_conn, closing(pg_conn.cursor()) as pg_cursor:
-            pg_cursor.execute(query)
+            extras.execute_batch(cur=pg_cursor, sql=query, argslist=records)
             pg_conn.commit()
 
     def insert_user_data(self, user_data: dict) -> None:
@@ -33,17 +35,10 @@ class PostgresDB:
                 , username
                 , is_premium
                 , url
-            ) VALUES (
-                  {user_id}
-                , {is_bot}
-                , '{first_name}'
-                , '{last_name}'
-                , '{username}'
-                , {is_premium}
-                , '{url}'
-            );
+            ) VALUES (%s,%s,%s,%s,%s,%s,%s);
         """
-        self._execute_query(
-            query=query.format(**user_data)
+        self._execute_batch(
+            query=query.format(**user_data),
+            records=[user_data]
         )
         logger.info("User_data successfully inserted into db.")

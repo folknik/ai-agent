bb895852f7629994a4a842ed0c15bc0705161de7
Алексей Фадеев <a.fadeev@dataintegral.ru>
2025-06-15 18:57:50 +0300

    add chats table

--- create-db.sql
+++ create-db.sql
@@ -6,11 +6,20 @@ create schema if not exists users;
 create table if not exists users.user_requests (
     id              serial primary key,
     user_id         int8 not null,
+    chat_id         int8 not null,
     is_bot          boolean not null,
     first_name      varchar not null,
     last_name       varchar null,
     username        varchar null,
     is_premium      boolean null,
     url             varchar not null,
+    created_at      timestamptz default now() not null
+ );
+
+
+create table if not exists users.chats (
+    id              serial primary key,
+    chat_id         int8 not null,
+    user_id         int8 not null,
     created_at      timestamptz default now() not null
  );
